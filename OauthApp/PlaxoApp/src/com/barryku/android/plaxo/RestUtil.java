d52588365450386806a176e3fbeda2e2413199b3
unknown <bku@.aligntech.com>
2011-06-16 10:05:05 -0700

    added android app using scribe

---
+++ OauthApp/PlaxoApp/src/com/barryku/android/plaxo/RestUtil.java
@@ -0,0 +1,40 @@
+package com.barryku.android.plaxo;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import android.util.Log;
+
+public class RestUtil {
+	public static RestTemplate getRestTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		FormHttpMessageConverter formConverter = new FormHttpMessageConverter();
+		formConverter.setCharset(Charset.forName("UTF8"));
+		restTemplate.getMessageConverters().add(formConverter);
+		
+		restTemplate.setErrorHandler(new ResponseErrorHandler() {
+			
+			public boolean hasError(ClientHttpResponse resp) throws IOException {	
+				HttpStatus status = resp.getStatusCode();
+				if (HttpStatus.CREATED.equals(status) || HttpStatus.OK.equals(status)) {
+					return false;
+				} else {
+					Log.d("mydebug", "response: " + resp.getBody());
+					return true;
+				}
+			}
+			
+			public void handleError(ClientHttpResponse resp) throws IOException {
+				throw new HttpClientErrorException(resp.getStatusCode());
+			}
+		});
+    	return restTemplate;
+	}
+}
